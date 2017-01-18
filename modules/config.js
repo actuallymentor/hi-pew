82347667e02cb8a24852852e4ce360bc9d7b8d23
Mentor <mentor@palokaj.co>
2017-01-18 20:24:01 +0100

    Document config

--- modules/config.js
+++ modules/config.js
@@ -1,9 +1,11 @@
 module.exports = {
+	// Identity variables used in pug templates
 	identity: {
 		title: "Website",
 		desc: "Description of website",
 		"logo": "logo.jpg"
 	},
+	// System vars managing some pug elements as well as file paths
 	system: {
 		public: __dirname + '/../public/',
 		source: __dirname + '/../src/',
@@ -11,6 +13,7 @@ module.exports = {
 		gverification: undefined,
 		year: new Date().getFullYear()
 	},
+	// About the author. Change this to your own unless you went me to get credit for your page of course... <3
 	author: {
 		firstname: "Mentor",
 		lastname: "Palokaj",
@@ -20,6 +23,7 @@ module.exports = {
 		facebook: "1299359953416544",
 		url: "https://www.skillcollector.com/"
 	},
+	// Tracking codes
 	track: {
 		ga: "UA-XXXXXXXX-XX"
 	}
