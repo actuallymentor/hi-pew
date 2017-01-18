bfd7aa16219e580e591369502a76582ba07d69c0
Mentor <mentor@palokaj.co>
2017-01-18 22:10:14 +0100

    Expand pug reader doc

--- modules/parse-pugfiles.js
+++ modules/parse-pugfiles.js
@@ -5,6 +5,7 @@ const getpugs = path => {
 	return new Promise( ( resolve, reject ) => {
 		fs.readdir( path, ( err, files ) => {
 			if ( err ) return reject( err )
+			// This will return an array of file names that contain .pug
 			resolve( files.filter( file => { return file.indexOf( '.pug' ) != -1 } ) )
 		} )
 	} )
@@ -15,6 +16,7 @@ const readdata = ( path, filename ) => {
 	return new Promise( ( resolve, reject ) => {
 		fs.readFile( path + filename, 'utf8', ( err, data ) => {
 			if ( err ) return reject( err )
+			// Resolve with an object that contains the name and data of a file
 			resolve( { filename: filename, data: data } )
 		} )
 	} )
@@ -23,7 +25,9 @@ const readdata = ( path, filename ) => {
 // Use the above two promises to return the pug files ( as pug syntax )
 const returnpugs = path => {
 	return new Promise( ( resolve, reject ) => {
+		// Grab all .pug files
 		getpugs( path ).then( files => {
+			// Grab the content of all .pug files
 			return Promise.all( files.map( filename => { return readdata( path, filename ) } ) )
 		} ).then( resolve ).catch( reject )
 	} )
