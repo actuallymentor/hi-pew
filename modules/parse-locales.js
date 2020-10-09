c3ab4d31e5774326cbd99c0fb4566f702c657347
Mentor Palokaj <mentor@palokaj.co>
2020-10-09 10:59:11 +0200

    Allow es modules

--- modules/parse-locales.js
+++ modules/parse-locales.js
@@ -1,35 +1,31 @@
-const { promises: fs } = require( 'fs' )
+const fs = require( 'fs' )
 const { readFile } = require( './parse-fs' )
 
 // Grab all pug files from the root of the source directory
-const getJSONs = async path => {
-
-	try {
-		const files = await fs.readdir( path )
-		return files.filter( file => file.includes( '.json' ) )
-	} catch( e ) {
-		console.error( `Error reading ${path}: `, e )
-	}
-
-}
+const getJSONs = path => new Promise( ( resolve, reject ) => {
+	fs.readdir( path, ( err, files ) => {
+		if ( err ) return reject( err )
+		// This will return an array of file names that contain .pug
+		resolve( files.filter( file => { return file.includes( '.json' ) || file.includes( '.js' ) } ) )
+	} )
+} )
 
 // Validate the structure of the json filename and get the lang for it
 // Json file syntax should be language.json ( e.g. en.json or nl.json )
-const validateJson = async json => {
-
-	if( json.lang && json.slug.includes( '/' ) ) return json
-	else throw 'Invalid json'
-
-}
+const validateJson = json => new Promise( ( resolve, reject ) => json.lang && json.slug.includes( '/' ) && resolve( json ) || reject( 'Invalid json' ) )
 
 // Return the json files ( as pug syntax )
 // Grab all .json files
 const getContent = path => getJSONs( path )
 // Grab the content of all .json files
 // Get the content of each file with it's language string, outputs { filename, content }
-.then( files => Promise.all( files.map( file => readFile( path, file ) ) ) )
-// Extract json data from strings
-.then( strings => strings.map( string => JSON.parse( string.data ) ) )
+.then( files => Promise.all( files.map( async file => {
+	// Load js module
+	if( file.includes( 'js' ) ) return require( `${path}/${file}` )
+	// Extract json data from strings
+	return readFile( path, file ).then( string =>  JSON.parse( string.data ) )
+} ) ) )
+
 // Validate that the jsons are well-formatted
 .then( allJsons => Promise.all( allJsons.map( json => validateJson( json ) ) ) )
 
--- src/content/default.js
+++ src/content/default.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
 	"slug": "/",
 	"lang": "en"
 }
