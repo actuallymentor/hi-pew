c1264299aeb42298d891ed918b420f016e4204c4
Mentor Palokaj <mentor@palokaj.co>
2020-10-09 15:58:02 +0200

    Fix asset issue

--- modules/publish-assets.js
+++ modules/publish-assets.js
@@ -1,11 +1,12 @@
 // promise based file parsing
-const pfs = require( __dirname + '/parse-fs' )
+const { del, mkdir } = require( __dirname + '/parse-fs' )
 // Recursive copy library
 const ncp = require( 'ncp' )
 
 let copyfolder = ( source, destination ) => {
-	return new Promise( ( resolve, reject ) => {
+	return new Promise( async ( resolve, reject ) => {
 		// Recursively copy all assets from the source to the public folder
+		await mkdir( destination )
 		ncp( source, destination, err => {
 			if ( err ) return reject(  err )
 			resolve( )
@@ -13,7 +14,7 @@ let copyfolder = ( source, destination ) => {
 	} )
 }
 
-const copyassets = site => pfs.del( site.system.public + 'assets/*' )
+const copyassets = site => del( site.system.public + 'assets/*' )
 .then( copyfolder( site.system.source + 'assets', site.system.public + 'assets' ) )
 
 module.exports = copyassets
